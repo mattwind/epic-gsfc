9bb8971a45edc0c51badc71640aaeedcb1dc9e0a
Matt Wind <mattwind@canara.com>
2018-05-08 14:59:35 -0700

    downloading image 16

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace epic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = @"https://epic.gsfc.nasa.gov/";
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(html);
+            var xpath = "//*[@id='info']/div[16]/span[1]/a";
+            var node = htmlDoc.DocumentNode.SelectSingleNode(xpath);
+            Console.WriteLine(node.Attributes["href"].Value);
+        }
+    }
+}
